6d45eb36f03a6f9d2e20060ca3388c942066d710
SY1CRT <subhanyasin99@hotmail.com>
2025-12-16 10:25:01 +0000

    added gitignore file

--- w2s1.py
+++ w2s1.py
@@ -7,4 +7,15 @@
 # print ("hello", name, "you are", age, "years old")
 
 # activity 2
+# a = int(input("enter first number"))
+# b = int(input("enter second number"))
+# print("sum is",str(a + b), "and product is", str(a * b))
+
+# activity 6
+# name = str(input("enter product name:"))
+# price = float(input("enter price:"))
+# quantity = int(input("enter quantity"))
+# total_cost = price * quantity
+# print("you bought", quantity, "of", name)
+# print("total cost is", total_cost)
 

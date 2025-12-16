17bee01a28e1a4b5920fe09f0f8a2a13210505e4
SY1CRT <subhanyasin99@hotmail.com>
2025-12-16 14:05:51 +0000

    added gitignore file

---
+++ w2s2 afternoon practice problems.py
@@ -0,0 +1,53 @@
+# age = 22
+# name = "subhan"
+#
+# print(name)
+# print(name.upper())
+# print(name.lower())
+# print(name.strip())
+
+# activity 1
+
+# item_name = input("Enter item name: ")
+# price = float(input("Enter price: "))
+# quantity = int(input("Enter quantity: "))
+# print("You bought " + str(quantity) + " x " + item_name + " at £" + str(price) + " each. Total = £" + str(price * quantity))
+# print(f"You bought {quantity} x {item_name} at £{price} each. Total = £{price * quantity}")
+# age = int(input("Enter your age: "))
+# print("You are", age, "years old.")
+# print(f"You are {age} years old.")
+
+# activity 2
+
+# full_name = input("Enter your full name: ").strip()
+# course_name = input("Enter your course name: ").strip()
+# full_name = full_name.title()
+# course_name = course_name.upper()
+# print(f"Student: {full_name} ({course_name})")
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# course = input("Enter your course: ")
+# print(f"Your details:\nName: {name}\nAge: {age}\nCourse: {course}")
+# template = "Hello NAME, welcome to COURSE."
+# name = input("Enter your name: ").strip()
+# course = input("Enter your course: ").strip()
+# message = template.replace("NAME", name).replace("COURSE", course)
+# print(message)
+# sentence = input("Type a sentence: ")
+# print(sentence.upper())
+# print(sentence.lower())
+# print(len(sentence))
+
+# activity 3
+
+
+
+
+
+
+
+
+
+
+
+

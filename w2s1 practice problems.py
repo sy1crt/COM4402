f7298c3c2eb1aa083cb602821c7f18e19abfd221
SY1CRT <subhanyasin99@hotmail.com>
2025-12-16 13:07:51 +0000

    added gitignore file

---
+++ w2s1 practice problems.py
@@ -0,0 +1,16 @@
+#activity 8
+
+# name = str(input("enter your name"))
+# age = int(input("enter age:"))
+# height = float(input("enter height"))
+# age_plus_one = age + 1
+# print("hello")
+# print("next year you will be", age_plus_one)
+# print("your height is", height, "metres")
+
+#activity 10
+
+# name = str(input("enter your name: "))
+# age = int(input("enter your age: "))
+# age_next_year = age + 1
+# print("hello", name, "next year you will be", age_next_year)
